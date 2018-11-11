90ea134dd9771b01a199fbe82bd584ae1fcab2cd
soruma <soruma.net@gmail.com>
2018-11-11 11:47:50 +0900

    remove unnecessary auto generated files

--- spec/decorators/order_detail_decorator_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-describe OrderDetailDecorator do
-  let(:order_detail) { OrderDetail.new.extend OrderDetailDecorator }
-  subject { order_detail }
-  it { should be_a OrderDetail }
-end
