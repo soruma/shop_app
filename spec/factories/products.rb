3a7683a893f7dea48fce40cf27f61c2ae1531681
soruma <soruma.net@gmail.com>
2018-11-11 11:50:53 +0900

    invalid datum pattern

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
   factory :product do
     name { Faker::Movie.quote }
     price { Faker::Number.number(5) }
+
+    trait :invalid do
+      name { '' }
+      price { 0 }
+    end
   end
 end
